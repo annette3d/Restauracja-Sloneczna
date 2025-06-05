6540d2670e8b83a5040621cf5c95d4a8e40a02fd
annette3d <148874372+annette3d@users.noreply.github.com>
2025-06-05 16:37:26 +0200

    Add files via upload

---
+++ kontakt (1).php
@@ -0,0 +1,23 @@
+﻿<?php
+require 'db.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $imie = $_POST['imie'];
+    $email = $_POST['email'];
+    $wiadomosc = $_POST['wiadomosc'];
+
+    $sql = "INSERT INTO kontakt (imie, email, wiadomosc)
+            VALUES (:imie, :email, :wiadomosc)";
+    $stmt = $pdo->prepare($sql);
+
+    try {
+        $stmt->execute([
+            ':imie' => $imie,
+            ':email' => $email,
+            ':wiadomosc' => $wiadomosc
+        ]);
+        echo "✅ Wiadomość została wysłana!";
+    } catch (Exception $e) {
+        echo "❌ Błąd: " . $e->getMessage();
+    }
+}
